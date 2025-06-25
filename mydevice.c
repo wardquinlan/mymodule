309c59ce62c1b112cfba4ef77763cfe8ec1f6018
wardquinlan <wardquinlan@outlook.com>
2025-06-25 08:32:13 -0400

    continuation of my device

--- mydevice.c
+++ mydevice.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/uaccess.h>
 #include <linux/fs.h>
+#include <linux/device.h>
 #include <linux/proc_fs.h>// Module metadata
 
 MODULE_AUTHOR("Ward Quinlan");
@@ -11,6 +12,10 @@ MODULE_LICENSE("GPL");// Custom init and exit methods
 // Based on sample code taken from:
 //
 // https://sysprog21.github.io/lkmpg/#the-fileoperations-structure
+
+#define DEVICE_NAME "mydevice"
+static int major;
+static struct class *cls;
 
 static ssize_t custom_read(struct file* file, char __user* user_buffer, size_t count, loff_t* offset)
 {
@@ -35,13 +40,26 @@ static struct file_operations chardev_fops = {
 
 static int __init custom_init(void)
 {
-	printk(KERN_INFO "My kernel device loaded\n");
+	printk(KERN_INFO "registering character device\n");
+	// passing 0 as the major number means ask it to dynamically assign a major device number
+	major = register_chrdev(0, DEVICE_NAME, &chardev_fops);
+	printk(KERN_INFO "assigned major number %d\n", major);
+
+	// allocate the class
+	printk(KERN_INFO "allocating class\n");
+	cls = class_create(DEVICE_NAME);
+
+	printk(KERN_INFO "my kernel device loaded\n");
 	return 0;
 }
 
 static void __exit custom_exit(void)
 {
-	printk(KERN_INFO "My kernel device unloaded\n");
+	//device_destroy(cls, MKDEV(major, 0)); 
+	printk(KERN_INFO "releasing class\n");
+	class_destroy(cls); 
+
+	printk(KERN_INFO "my kernel device unloaded\n");
 }
 
 module_init(custom_init);
